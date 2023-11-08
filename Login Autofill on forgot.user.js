b2c562e2accd836a65d509c1146703ae4ca379e4
Morgulisan <malte.ktz@hotmail.de>
2023-11-09 00:36:47 +0100

    Update Tesla Autofill on Login to work with redirects to local language

--- Login Autofill on forgot.user.js
+++ Login Autofill on forgot.user.js
@@ -1,18 +1,39 @@
 // ==UserScript==
-// @name         Login Autofill on forgot
+// @name         Tesla Login Autofill on forgot
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Autofill email if provided as parameter
 // @author       Malte Kretzschmar
-// @match        https://auth.tesla.com/user/password/forgot*
+// @match        https://auth.tesla.com/*/user/password/forgot*
 // @icon         https://www.google.com/s2/favicons?domain=tesla.com
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.addEventListener("load",function (e) {
-    const urlParams = new URLSearchParams(window.location.search);
-    document.querySelector('#form-input-identity').value = urlParams.get('form-input-identity');});
+
+    // Function to handle autofill.
+    function autofillEmail() {
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const emailInput = document.querySelector('#form-input-identity');
+        // If the input field exists and the parameter is provided, fill it.
+        if (emailInput && urlParams.get('form-input-identity')) {
+            emailInput.value = urlParams.get('form-input-identity');
+        }
+    }
+
+    // Wait until the window is loaded, then introduce a delay.
+    window.addEventListener("load", function() {
+        setTimeout(autofillEmail, 1000); // Delay of 1000ms (1 second).
+        setTimeout(autofillEmail, 2000); // Delay of 2000ms (2 seconds).
+    });
+
+    // Retry autofill upon user click anywhere on the document.
+    // Adjust the selector as needed if a more specific element is preferred.
+    document.addEventListener('click', function(e) {
+        autofillEmail();
+    });
+
     // Your code here...
 })();
