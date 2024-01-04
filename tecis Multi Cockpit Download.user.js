4cc8d0a88d675112f32e8a6314585f631a1e858c
Morgulisan <malte.ktz@hotmail.de>
2024-01-04 11:23:58 +0100

    Add Multi Cockpit Download and VersDiagnose Detailer

---
+++ tecis Multi Cockpit Download.user.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         tecis Multi Cockpit Download
+// @namespace    http://tampermonkey.net/
+// @version      2024-01-03
+// @description  Download multiple Cockpits
+// @author       Malte Kretzschmar
+// @match        https://vertrieb.tecis.de:11059/gr/reporting/zeigeBeurteilungsbogenForm.do
+// @match        https://www.tecis.de/gr/reporting/zeigeBeurteilungsbogenForm.do
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tecis.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+/**
+document.querySelector("#reporting_main_content > table > tbody > tr > td:nth-child(4) > table > tbody > tr:nth-child(3) > td:nth-child(1) > form").target = "transFrame";
+
+// Create the iframe element
+var iframe = document.createElement('iframe');
+iframe.style.display = 'none';
+iframe.name = 'transFrame';
+iframe.id = 'transFrame';
+
+// Get the reference element where you want to insert the iframe as a sibling
+var refElement = document.querySelector("#reporting_main_content > table > tbody > tr > td:nth-child(4) > table > tbody > tr:nth-child(3) > td:nth-child(1) > form");
+
+// Insert the iframe as a sibling to the reference element
+refElement.parentNode.insertBefore(iframe, refElement.nextSibling);
+**/
+
+let loc = document.querySelector("#reporting_main_content > table > tbody > tr > td:nth-child(4) > table > tbody > tr:nth-child(3) > td:nth-child(1) > div");
+let area = document.createElement("textarea");
+area.id = "listVPNRFranz";
+area.style.height = "200px";
+loc.appendChild(area);
+let btn = document.createElement("button");
+btn.innerHTML = "Start";
+btn.onclick = () => {
+    console.log(document.getElementById("listVPNRFranz").value);
+    let list = document.getElementById("listVPNRFranz").value.split('\n');
+    go(list);
+    btn.disabled = true;
+}
+loc.appendChild(btn);
+
+function go(list) {
+    if(list.length > 0) {
+        document.querySelector("#reporting_main_content > table > tbody > tr > td:nth-child(4) > table > tbody > tr:nth-child(3) > td:nth-child(1) > form > table > tbody > tr:nth-child(3) > td > input[type=text]").value = list.pop();
+        document.querySelector("#reporting_main_content > table > tbody > tr > td:nth-child(4) > table > tbody > tr:nth-child(3) > td:nth-child(1) > form > table > tbody > tr:nth-child(4) > td > input[type=submit]").click();
+        try {
+            document.querySelector("#ajaxBusy").remove();
+        } catch (e) {}
+        setTimeout(go, 1500, list);
+    }
+    else {
+        btn.disabled = false;
+    }
+}
+
+})();
---
+++ tecis versDiagnose Details.user.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         tecis versDiagnose Details
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the world!
+// @author       Malte
+// @match        https://tecis.versdiagnose.de/~riskassessment/assessment_results.do
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=versdiagnose.de
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+
+// Parse the string into an HTML document
+    var parser = new DOMParser();
+
+
+    function replace() {
+        let buttons = document.querySelector("#assessment-results-table-target > div:nth-child(2) > div > div").getElementsByTagName("button");
+        if(buttons.length > 0){
+            buttons[0].click();
+            setTimeout(()=>{
+                //copy Content
+                let save = document.querySelector("#summary_section > div.row.hidden-print > div.col-xs-12.col-md-8").innerHTML;
+                //close Modal
+                document.querySelector("#vis > div.modal.js-modal-lg.in > div > div > div.modal-header > button").click();
+
+                // Parse the string into an HTML document
+                let doc = parser.parseFromString(save, 'text/html').body.firstChild;
+
+                buttons[0].parentNode.replaceChild(doc , buttons[0]);
+                setTimeout(replace,200);
+            }, 800);
+        }
+    }
+
+    replace();
+})();
