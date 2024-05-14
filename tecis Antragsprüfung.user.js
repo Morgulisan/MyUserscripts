4b3574f3c393bbda88aac71a9368cef2a9daff28
Morgulisan <malte.ktz@hotmail.de>
2024-05-14 15:08:44 +0200

    Antragsprüfungstool FodB ergänzt.

--- tecis Antragsprüfung.user.js
+++ tecis Antragsprüfung.user.js
@@ -14,7 +14,7 @@
 (function() {
     'use strict';
 
-    let invest = ["Universal Investment Gesellschaft mbH","Sarasin Wertpapierhandelsbank AG","Amundi Asset Management (PLC)","Sparinvest", "JPMorgan Asset Management", "DWS Investment GmbH", "BlackRock", "AllianceBernstein", "Vontobel", "Pictet", "Comgest", "Schroder Investment", "Gutmann Kapitalanlageaktiengesellschaft", "Robeco"]; // column 6
+    let invest = ["Universal Investment Gesellschaft mbH","Sarasin Wertpapierhandelsbank AG","Amundi Asset Management (PLC)","Sparinvest", "JPMorgan Asset Management", "DWS Investment GmbH", "BlackRock", "AllianceBernstein", "Vontobel", "Pictet", "Comgest", "Schroder Investment", "Gutmann Kapitalanlageaktiengesellschaft", "Robeco", "Flossbach von Storch Invest S.A", "FundRock Management Company S.A.", "iShares - BlackRock (Public Limited)"]; // column 6
     let immo = ["DOMCURA AG","Domicil Objekt GmbH & Co. KG"]; // column 6
     let Tarif = ["Multidepot","FodB Depoteröffnung"]; // column 7
 
