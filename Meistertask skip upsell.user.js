206f81d83753f0b4a41d90f935a0a42228d824e8
Morgulisan <malte.ktz@hotmail.de>
2025-12-15 09:44:57 +0100

    Update `Meistertask skip upsell` script:
    - Add new URL match for pricing page.
    - Include metadata fields: `updateURL` and `downloadURL`.
    - Adjust script execution timing to `document-start`.
    - Increment version to 1.0.0.

--- Meistertask skip upsell.user.js
+++ Meistertask skip upsell.user.js
@@ -1,12 +1,16 @@
 // ==UserScript==
 // @name         Meistertask skip upsell
 // @namespace    http://tampermonkey.net/
-// @version      2025-11-14
+// @version      1.0.0
 // @description  Meistertask leitet auf "Jetzt zahlen" beim ersten aufruf weiter, von dort leiten wir einfach weg aufs Dashboard
 // @author       Malte Kretzschmar
+// @run-at       document-start
 // @match        https://www.meistertask.com/pages/de/version1
 // @match        https://www.meistertask.com/de
+// @match        https://accounts.meister.co/payments/mt/en/pricing
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=meistertask.com
+// @updateURL    https://mopoliti.de/Userscripts/Meistertask%20skip%20upsell.user.js
+// @downloadURL  https://mopoliti.de/Userscripts/Meistertask%20skip%20upsell.user.js
 // @grant        none
 // ==/UserScript==
 
